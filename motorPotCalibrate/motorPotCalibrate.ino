17aede534b185ca621690bc0c2825ef9f36c30ae
greenteawarrior <emily.wang@students.olin.edu>
2013-10-01 16:56:54 -0400

    calibration in process

--- motorPotCalibrate/motorPotCalibrate.ino
+++ motorPotCalibrate/motorPotCalibrate.ino
@@ -1,5 +1,18 @@
+// still a work in progress
+
 // Emily Wang and Claire Diehl
 // POE Lab 2 : Controlling a motor
+
+// Calibrating the motor and potentiometer
+// aka. mapping potentiometer voltages to 
+// motor position (angles in degrees)
+
+// Potentiometer things
+const int potPin = 3;    // this is the input pin for the potentiometer
+int potVal = 0;       // variable store the voltage from the potentiometer
+
+// Motor position things
+int motorStep = 100; //vary this for each trial to get calibration data
 
 
 // Stepper motor things 
@@ -13,26 +26,9 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 // Or, create it with a different I2C address (say for stacking)
 // Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
 
-// Connect a stepper motor with 512 steps per revolution (1.8 degree)
-// to motor port #1 (M3 and M4)
+// Connect a stepper motor with 512 steps per 
+// revolution (1.8 degree) to motor port #1 (M3 and M4)
 Adafruit_StepperMotor *myMotor = AFMS.getStepper(512, 1);
-
-
-// Potentiometer things
-const int potPin = 3;    // this is the input pin for the potentiometer
-int potVal = 0;       // variable store the voltage from the potentiometer
-
-
-// control loop variables - do we want integers or floats?
-int actualAngle; //potPos converted into angles (degrees or radians?)
-int desiredAngle;
-int error; //subtraction: desired angle - current angle
-
-int Kp; //proportional constant term
-int angV; //instantaneous angular velocity of the motor
-int Kd; //derivative constant term
-int PDsum; //sum: propotional gain + derivative gain
-
 
 void setup() {
   Serial.begin(9600);
@@ -48,8 +44,10 @@ void loop() {
   Serial.println(potVal);
   
   //Single coil steps
-  Serial.println("Single coil steps");
-  myMotor->step(100, FORWARD, SINGLE); 
-  myMotor->step(100, BACKWARD, SINGLE); 
-}
- 
+  myMotor->step(motorStep, FORWARD, SINGLE); 
+  myMotor->step(motorStep, BACKWARD, SINGLE);
+  Serial.println('motorStep'); 
+  Serial.println('Record the angle!');
+  
+  //FIGURE OUT WHICH DIRECTION IS WHICH (forwards or backwards)
+  //print out a compass to put on to the output shaft
