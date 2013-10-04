465a65377b4116cbcdbdbcf2495ce65cecb68af0
greenteawarrior <emily.wang@students.olin.edu>
2013-10-04 15:08:09 -0400

    whee proprtional control things

--- PIDLoop/PIDLoop.ino
+++ PIDLoop/PIDLoop.ino
@@ -1,35 +1,25 @@
 // Emily Wang and Claire Diehl
 // POE Lab 2 : Controlling a motor
 
-// Stepper motor things 
-// (shoutout to Adafruit and their "StepperTest" example code)
+// Stepper motor things (shoutout to Adafruit and their "StepperTest" example code)
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_PWMServoDriver.h"
 
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
-// Or, create it with a different I2C address (say for stacking)
-// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
-
-// Connect a stepper motor with 512 steps per revolution (1.8 degree) to motor port #1
 Adafruit_StepperMotor *myMotor = AFMS.getStepper(512, 1);
-
 
 // Potentiometer things
 const int potPin = 3;    // this is the input pin for the potentiometer
-int potVal = 0;       // variable store the voltage from the potentiometer
+const int potStop0 = 0; //potStop0 and potStop1 is the movable angle range when turning potentiometer
+const int potStop1 = 270;
+const float p2g = 3.79; // = 1023/270 converstion factor from potentiometer measurement (potVal) to degrees
+int potVal = 0;       // variable store the potentiometer's analogRead() measurement 
 
-// control loop variables - do we want integers or floats?
-int actualAngle; //potPos converted into angles (degrees or radians?)
-int desiredAngle;
-int error; //subtraction: desired angle - current angle
-
-int Kp; //proportional constant term
-int angV; //instantaneous angular velocity of the motor
-int Kd; //derivative constant term
-int PDsum; //sum: propotional gain + derivative gain
-
+int actualAngle; //potVal converetd to angles (degrees)
+int desiredAngle; //specified by user
+int error; // subtraction: desiredAngle - currentAngle
 
 void setup() {
   Serial.begin(9600);
@@ -38,12 +28,15 @@ void setup() {
   AFMS.begin();  // create with the default frequency 1.6KHz
   myMotor->setSpeed(10);  // 10 rpm   
   
+  
 }
 
 void loop() {
   potVal = analogRead(potPin);    // read value from potentiometer
   Serial.println("potVal:");
   Serial.println(potVal);
+  
+  
   
   
 
