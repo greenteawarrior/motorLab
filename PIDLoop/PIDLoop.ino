6e169c7f4bb45bd6daabc8d6aed9657ded725cf3
greenteawarrior <emily.wang@students.olin.edu>
2013-10-08 04:05:52 -0400

    cleaning up code and working on writeup

--- PIDLoop/PIDLoop.ino
+++ PIDLoop/PIDLoop.ino
@@ -17,8 +17,12 @@ const int potStop1 = 270;
 const float p2g = 0.263; // = 270/1023 converstion factor from potentiometer measurement (potVal) to degrees
 float potVal = 0;       // variable store the potentiometer's analogRead() measurement 
 
+// Angular position things
 int actualAngle; //potVal converted to angles (degrees); this is the current angular position of the motor
-const int desiredAngle = 170; //specified by user, any number between 130 and 270
+int desiredAngle; //specified by user, any number between 130 and 270
+//change back to const int if desired
+
+// Motor movement variables
 boolean motorDir; //FORWARD or BACKWARD
 int error; // subtraction: desiredAngle - currentAngle
 
@@ -26,11 +30,11 @@ float stepsToNextAngleProp; //proportional control calculation
 float stepsToNextAngleDer; //derivative control calculation
 float stepsToNextAngle;
 
-//Control constants
+// Control constants
 float kP = .5; //proportional constant term
 float kD = 50; //derivative constant term
 
-//Derivative Control Terms
+// Derivative Control Terms
 float errorArray[4]={0,0,0,0};
 float errorDerivative;
 float total;
@@ -38,6 +42,7 @@ int i; //for loop things
 long oldTime;
 long newTime;
 long passedTime;
+long currentTime;
 
 void setup() {
   Serial.begin(9600);
@@ -45,19 +50,25 @@ void setup() {
   
   AFMS.begin();  // create with the default frequency 1.6KHz
   myMotor->setSpeed(10);  // 10 rpm   
+  
+  potVal = analogRead(potPin);   
+  actualAngle = potVal * p2g;
+  Serial.println(actualAngle);
+  Serial.println("");
+  desiredAngle = actualAngle + 90;
 }
 
 void loop() {
-  //Read value of potentiometer and convert it to angles and obtain an error value.
+  // Read value of potentiometer and convert it to angles and obtain an error value.
   potVal = analogRead(potPin);   
   actualAngle = potVal * p2g;
   error = desiredAngle - actualAngle;
   
-  //Proportional Control
+  // Proportional Control
   stepsToNextAngleProp = kP * error;
   stepsToNextAngleProp = abs(stepsToNextAngleProp);
   
-  //Derivative Control
+  // Derivative Control
   newTime=millis();
   passedTime = newTime - oldTime;
   if (passedTime >= 20){
@@ -73,8 +84,8 @@ void loop() {
     Serial.println("");
     for (i=0;i<4;i++){
       total=total+errorArray[i];
-      Serial.println("arrayTotal");
-      Serial.println(total);
+      //Serial.println("arrayTotal");
+      //Serial.println(total);
     oldTime = newTime;
     }
     errorDerivative=total/(400);
@@ -86,7 +97,7 @@ void loop() {
   // Add the proportional and derivative things
   stepsToNextAngle = stepsToNextAngleProp + stepsToNextAngleDer;
  
-  //Move the motor!!
+  // Move the motor!!
   if (error >= 0){
     myMotor->step(stepsToNextAngle, FORWARD, SINGLE); 
     Serial.println("FORWARD");
@@ -100,22 +111,28 @@ void loop() {
   else if (error == 0) {
     myMotor->step(0, BACKWARD, SINGLE);      
   }
-   
-  Serial.println("potVal:");
-  Serial.println(potVal);
+  
+  // Print everything!
+  currentTime = millis();
+  Serial.println("currentTime:");
+  Serial.println(currentTime);
+  
+  //Serial.println("potVal:");
+  //Serial.println(potVal);
   Serial.println("actualAngle");
   Serial.println(actualAngle);
-  Serial.println("desiredAngle");
-  Serial.println(desiredAngle);
-  Serial.println("error");
-  Serial.println(error);
-  Serial.println("stepsToNextAngleProp:");
-  Serial.println(stepsToNextAngleProp);
-  Serial.println("stepsToNextAngleDer:");
-  Serial.println(stepsToNextAngleDer);
-  Serial.println("stepsToNextAngle:");
-  Serial.println(stepsToNextAngle);
+  //Serial.println("desiredAngle");
+  //Serial.println(desiredAngle); //currently 285
+  //Serial.println("error");
+  //Serial.println(error);
+  //Serial.println("stepsToNextAngleProp:");
+  //Serial.println(stepsToNextAngleProp);
+  //Serial.println("stepsToNextAngleDer:");
+  //Serial.println(stepsToNextAngleDer);
+  //Serial.println("stepsToNextAngle:");
+  //Serial.println(stepsToNextAngle);
   Serial.println();
   
 }
  
+
